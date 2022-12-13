a10cd389d6536a0e0eff5b34466776a99fdda442
cantan88 <cantan@naver.com>
2022-12-13 15:27:43 +0900

    함수 연습6

--- function1.py
+++ function1.py
@@ -13,3 +13,33 @@ def setValue(newValue):
 retValue = setValue(5)
 print(retValue)
 
+print('---불변형식---')
+a = 1.2
+print( id(a))
+a = 2.3
+print( id(a))
+
+print('---가변형식---')
+lst = [1, 2, 3]
+print( id(lst))
+lst.append(4)
+print( id(lst))
+
+
+print('---이름 해석 순서---')
+def func1(a):
+    x=1
+    return x+a
+
+#호출
+print( func1(1))
+
+x =5
+def func2(a):
+    return x+a
+
+#호출
+print( func2(1))
+
+
+
